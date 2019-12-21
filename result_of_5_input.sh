9dc55d443d5cef138e9343bc197dc4fbc3d844ab
Vivian Losciale <vivian.losciale@etu.univ-cotedazur.fr>
2019-12-21 13:42:39 +0100

    update script

--- result_of_5_input.sh
+++ result_of_5_input.sh
@@ -1,5 +1,5 @@
 cd ../ps5-projet3-1920-stormbreakers
-cat input_files/input1.in | mvn exec:java > ../verificator-projet3-ps5/resultat/Result_input1.txt
+cat input_files/input1.in | mvn exec:java@run > ../verificator-projet3-ps5/resultat/Result_input1.txt
 rm ../verificator-projet3-ps5/resultat/finalFileInput1.in
 cat input_files/input1.in ../verificator-projet3-ps5/resultat/Result_input1.txt >> ../verificator-projet3-ps5/resultat/finalFileInput1.in
 cd ../verificator-projet3-ps5/src
@@ -8,7 +8,8 @@ cd ../bin
 java fr.unice.polytech.main.VerificatorMain<../resultat/finalFileInput1.in
 cd ../../ps5-projet3-1920-stormbreakers
 
-cat input_files/input2.in | mvn exec:java > ../verificator-projet3-ps5/resultat/Result_input2.txt
+cd ../ps5-projet3-1920-stormbreakers
+cat input_files/input2.in | mvn exec:java@run > ../verificator-projet3-ps5/resultat/Result_input2.txt
 rm ../verificator-projet3-ps5/resultat/finalFileInput2.in
 cat input_files/input2.in ../verificator-projet3-ps5/resultat/Result_input2.txt >> ../verificator-projet3-ps5/resultat/finalFileInput2.in
 cd ../verificator-projet3-ps5/src
@@ -17,6 +18,36 @@ cd ../bin
 java fr.unice.polytech.main.VerificatorMain<../resultat/finalFileInput2.in
 cd ../../ps5-projet3-1920-stormbreakers
 
+
+cd ../ps5-projet3-1920-stormbreakers
+cat input_files/input3.in | mvn exec:java@run > ../verificator-projet3-ps5/resultat/Result_input3.txt
+rm ../verificator-projet3-ps5/resultat/finalFileInput3.in
+cat input_files/input3.in ../verificator-projet3-ps5/resultat/Result_input3.txt >> ../verificator-projet3-ps5/resultat/finalFileInput3.in
+cd ../verificator-projet3-ps5/src
+find src *.java | xargs -i javac -d bin {}
+cd ../bin
+java fr.unice.polytech.main.VerificatorMain<../resultat/finalFileInput3.in
+cd ../../ps5-projet3-1920-stormbreakers
+
+cd ../ps5-projet3-1920-stormbreakers
+cat input_files/input4.in | mvn exec:java@run > ../verificator-projet3-ps5/resultat/Result_input4.txt
+rm ../verificator-projet3-ps5/resultat/finalFileInput4.in
+cat input_files/input4.in ../verificator-projet3-ps5/resultat/Result_input4.txt >> ../verificator-projet3-ps5/resultat/finalFileInput4.in
+cd ../verificator-projet3-ps5/src
+find src *.java | xargs -i javac -d bin {}
+cd ../bin
+java fr.unice.polytech.main.VerificatorMain<../resultat/finalFileInput4.in
+cd ../../ps5-projet3-1920-stormbreakers
+
+cd ../ps5-projet3-1920-stormbreakers
+cat input_files/input5.in | mvn exec:java@run > ../verificator-projet3-ps5/resultat/Result_input5.txt
+rm ../verificator-projet3-ps5/resultat/finalFileInput5.in
+cat input_files/input5.in ../verificator-projet3-ps5/resultat/Result_input5.txt >> ../verificator-projet3-ps5/resultat/finalFileInput5.in
+cd ../verificator-projet3-ps5/src
+find src *.java | xargs -i javac -d bin {}
+cd ../bin
+java fr.unice.polytech.main.VerificatorMain<../resultat/finalFileInput5.in
+cd ../../ps5-projet3-1920-stormbreakers
 #cat input_files/input3.in | mvn exec:java > ../Verificator/resultat/Result_input3.txt
 #cat input_files/input3.in ../Verificator/resultat/Result_input3.txt >> ../Verificator/resultat/finalFileInput3.in
 #cat input_files/input4.in | mvn exec:java > ../Verificator/resultat/Result_input4.txt
